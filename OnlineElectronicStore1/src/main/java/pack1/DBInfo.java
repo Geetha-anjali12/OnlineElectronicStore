8d6459c4366c2b334430f6f632aa553fb8a9ee79
Geethanjali Golla <gollageethanjali1@gmail.com>
2025-11-20 11:53:15 +0530

    new db

--- OnlineElectronicStore1/src/main/java/pack1/DBInfo.java
+++ OnlineElectronicStore1/src/main/java/pack1/DBInfo.java
@@ -2,9 +2,9 @@ package pack1;
 
 public interface DBInfo 
 {
-public static final String driver="oracle.jdbc.OracleDriver";
-public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl1";
-public static final String dbUname="mydb11am";
-public static final String dbPwd="123";
+public static final String driver="org.postgresql.Driver";
+public static final String dbUrl="postgresql://admin:n7j1YBIlqhvBRpmdJ0B0aCRFNx0wyAim@dpg-d4fb34euk2gs73f7q5m0-a.oregon-postgres.render.com/db_s1n8";
+public static final String dbUname="admin";
+public static final String dbPwd="n7j1YBIlqhvBRpmdJ0B0aCRFNx0wyAim";
 
 }
